9f8050f8fc544f9427c5f7b29274bf9dbf64663d
raf <lumang.rafael@gmail.com>
2024-02-26 15:37:42 -0800

    Updated project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,69 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid";
 
-import confetti from 'canvas-confetti';
+// console.log(uuidV4());
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+// on submit, if the input value is not null then create a newTask abiding by the Task type and add to list
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  //adds task to list
+  tasks.push(newTask);
+
+  //adds the task to the top of the page
+  addListItem(newTask);
+
+  //clear the input on submit
+  input.value = "";
+});
+
+// creates list items to add to the task list by takin the task and appending to list
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+
+  // everytime you click on checkbox, toggles completed to true or false
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+//save tasks to localStorage
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
